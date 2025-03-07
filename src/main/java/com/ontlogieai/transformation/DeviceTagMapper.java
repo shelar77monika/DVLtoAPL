37b86b5798fedf86aec353f8c723164dd9c4184a
gajanan999 <gajanangaikwad999@gmail.com>
2025-03-07 11:58:46 +0100

    changes did to refactor the code

--- src/main/java/com/ontlogieai/transformation/DeviceTagMapper.java
+++ src/main/java/com/ontlogieai/transformation/DeviceTagMapper.java
@@ -14,8 +14,7 @@ import java.util.*;
 public class DeviceTagMapper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DeviceTagMapper.class.getName());
-    private static final Map<String, String> STANDARD_DEVICE_TAGS = new HashMap<>();
-    private static final Map<String, List<String>> DEVICE_KEY_MAPPINGS = new HashMap<>();
+
     private static final Set<String> DEVICE_TYPES = Set.of(
             "TT", "FT", "MT", "PT", "ACU", "XC", "TC", "QIT", "UPS",
             "VAV", "XT", "XA", "FCV", "KS", "XI", "PMP"
@@ -26,58 +25,6 @@ public class DeviceTagMapper {
     public DeviceTagMapper(){
         configuration = ConfigurationReader.getConfiguration();
     }
-
-    static {
-        // Populate the standard device tag mapping
-        loadDeviceTags();
-        loadDeviceKeys();
-
-    }
-
-    private static void loadDeviceTags() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String resourcePath = "config.json"; // Can be changed to an external path
-
-        try (InputStream inputStream = DeviceTagMapper.class.getClassLoader().getResourceAsStream(resourcePath)) {
-            if (inputStream != null) {
-                Map<String, Object> rootMap = objectMapper.readValue(inputStream, new TypeReference<>() {});
-                Map<String, String> deviceTags = (Map<String, String>) rootMap.get("deviceTagMapping"); // Extract the nested map
-
-                if (deviceTags != null) {
-                    STANDARD_DEVICE_TAGS.putAll(deviceTags);
-                } else {
-                    LOGGER.warn("Key 'deviceTagMapping' not found in JSON.");
-                }
-            } else {
-                LOGGER.warn("Resource '{}' not found. Ensure the file is present in the resources folder.", resourcePath);
-            }
-        } catch (IOException e) {
-            LOGGER.error("Error loading device tags from JSON", e);
-        }
-    }
-
-    private static void loadDeviceKeys() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String resourcePath = "config.json"; // Can be changed to an external path
-
-        try (InputStream inputStream = DeviceTagMapper.class.getClassLoader().getResourceAsStream(resourcePath)) {
-            if (inputStream != null) {
-                Map<String, Object> rootMap = objectMapper.readValue(inputStream, new TypeReference<>() {});
-                Map<String, List<String>> deviceKeyMappings = (Map<String, List<String>>) rootMap.get("deviceKeyMapping"); // Extract the nested map
-
-                if (deviceKeyMappings != null) {
-                    DEVICE_KEY_MAPPINGS.putAll(deviceKeyMappings);
-                } else {
-                    LOGGER.warn("Key 'devicekeyMapping' not found in JSON.");
-                }
-            } else {
-                LOGGER.warn("Resource '{}' not found. Ensure the file is present in the resources folder.", resourcePath);
-            }
-        } catch (IOException e) {
-            LOGGER.error("Error loading device tags from JSON", e);
-        }
-    }
-
 
     public String getStandardDeviceTag(String deviceTag, String pointDescription) {
         if ("J460-02-2TT-717".equalsIgnoreCase(deviceTag)) {
